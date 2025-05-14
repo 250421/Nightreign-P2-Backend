e913ca899747b9db24b924045315f57c21717efd
ditran93 <ditran93@gmail.com>
2025-05-14 14:06:03 -0500

    fix: return game room id when create room

--- src/main/java/com/revature/battlesimulator/models/GameRoom.java
+++ src/main/java/com/revature/battlesimulator/models/GameRoom.java
@@ -16,14 +16,14 @@ public class GameRoom {
         this.id = UUID.randomUUID().toString().replaceAll("-", "").substring(0, 8);
         this.name = name;
         this.creator = creator;
-        this.players.add(creator); // Creator is also a player
+        this.players.add(creator);
     }
 
     public boolean addPlayer(GameRoomUser user) {
         // Check if the user is already in the room
         for (GameRoomUser player : players) {
             if (player.getUserId().equals(user.getUserId())) {
-                return false; // User already in the room
+                return false;
             }
         }
 
