6c2e232fd432920523444fa0a3cb22ae5cb23f8e
Jarrell Norwood <jnorwoo92@gmail.com>
2025-05-15 20:56:09 -0400

    battleresult changing fix

--- src/main/java/com/revature/battlesimulator/dtos/responses/BattleResult.java
+++ src/main/java/com/revature/battlesimulator/dtos/responses/BattleResult.java
@@ -1,30 +1,11 @@
 package com.revature.battlesimulator.dtos.responses;
 
+import lombok.Data;
+
+@Data
 public class BattleResult {
     private String winner;
     private String reason;
+    private String winningCharacter;
 
-    // Constructors
-    public BattleResult() {}
-    public BattleResult(String winner, String reason) {
-        this.winner = winner;
-        this.reason = reason;
-    }
-
-    // Getters and Setters
-    public String getWinner() {
-        return winner;
-    }
-
-    public void setWinner(String winner) {
-        this.winner = winner;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
 }
